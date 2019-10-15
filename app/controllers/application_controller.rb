f1e7152d192e439e14e59f2f7545b4201f81a242
Evgenia Ivanova <e.ivanova@potok.digital>
2019-10-15 10:23:43 +0300

    curl comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
+  # protect_from_forgery with: :null_session
+  # curl -i -H "Content-type: application/json" -X POST -d '{"data": {"type": "movements", "attributes": {"amount": "100", "datetime": "2019-08-21 23:03:24 +0300"}}, "controller": "movements", "action": "create" }'  http://localhost:3000/users/1/movements >> tmp/1.rby
+
   include Error::ErrorHandler
 end
